28141d97e0bfb7eb277b7241c53434e3316d5e82
Jose Damico <jose.damico@scicrop.com>
2023-10-21 11:47:18 -0300

    preparing for decode from soundcard

--- src/main/java/org/jdamico/javax25/App.java
+++ src/main/java/org/jdamico/javax25/App.java
@@ -1,5 +1,18 @@
 package org.jdamico.javax25;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.Properties;
+
+import org.jdamico.javax25.ax25.Afsk1200Modulator;
+import org.jdamico.javax25.ax25.Afsk1200MultiDemodulator;
+import org.jdamico.javax25.ax25.Packet;
+import org.jdamico.javax25.ax25.PacketDemodulator;
+import org.jdamico.javax25.radiocontrol.SerialTransmitController;
+import org.jdamico.javax25.radiocontrol.TransmitController;
+import org.jdamico.javax25.soundcard.Soundcard;
+
 /**
  * Hello world!
  *
@@ -8,6 +21,52 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Soundcard.enumerate();
+		SerialTransmitController.enumerate();
+
+		Properties p = System.getProperties();
+
+		int rate = 48000;
+		int filter_length = 32;
+
+		PacketHandlerImpl t = new PacketHandlerImpl();
+		Afsk1200Modulator mod = null;
+		PacketDemodulator multi = null;
+		try {
+			multi = new Afsk1200MultiDemodulator(rate,t);
+			mod = new Afsk1200Modulator(rate);
+		} catch (Exception e) {
+			System.out.println("Exception trying to create an Afsk1200 object: "+e.getMessage());
+		}
+		
+
+		/*** preparing to generate or capture audio packets ***/
+
+		String input = null;
+		String output = "PulseAudio Mixer";
+
+		int buffer_size = -1;
+		try {
+			// our default is 100ms
+			buffer_size = Integer.parseInt(p.getProperty("latency", "100").trim());
+		} catch (Exception e){
+			System.err.println("Exception parsing buffersize "+e.toString());
+		}
+
+		Soundcard sc = new Soundcard(rate,input,output,buffer_size,multi,mod);
+
+		if (p.containsKey("audio-level")) {
+			sc.displayAudioLevel();
+		}
+
+
+		/*** listen for incoming packets ***/
+
+		if (input != null) {
+		  System.out.printf("Listening for packets\n");
+  		//sc.openSoundInput(input);			
+	    sc.receive();
+		}
+		
     }
 }
